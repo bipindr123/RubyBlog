9f752047c740c837a625f3dc4f5d0adeffccb12b
bipindr123 <bipindr123@gmail.com>
2017-06-14 00:12:29 +0530

    some update

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,27 @@
+class PostsController < ApplicationController
+  def index
+    @myvar = "hello cous"
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
